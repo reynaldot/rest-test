0cd90467f11ac6f8af5656e73299f85907fdc567
Reynaldo Tortoledo <reynaldo.tortoledo@gmail.com>
2017-10-22 22:12:35 -0700

    update transactions module to fetch data from REST API

--- scripts/app.js
+++ scripts/app.js
@@ -13,11 +13,6 @@ APP.main = (() => {
   };
 
   const updateTable = (transactions) => {
-    console.assert(transactions.length === 3);
-    console.assert(transactions[0].Amount === '10,000.00');
-    console.assert(transactions[1].Amount === '5,000.00' );
-    console.assert(transactions[2].Amount === '2,000.00' );
-
     const tbody = document.querySelector('.transactions > tbody');
     tbody.innerHTML = transactions.map(createTransactionItem).join('');
   };
--- scripts/transactions.js
+++ scripts/transactions.js
@@ -1,53 +1,16 @@
 APP.transactions = (() => {
   'use strict';
 
+  const API_BASE_URL = 'http://resttest.bench.co';
   const FIRST_TRANSACTIONS_PAGE = 1;
 
-  const mockTransactionPages = {
-    1: {
-      totalCount: 3,
-      page: 1,
-      transactions: [
-        {
-          Date: "Jan 8th, 2014",
-          Company: "Adam's American Express",
-          Ledger: "Adjusting Entry",
-          Amount: "10,000.00"
-        }
-      ]
-    },
-    2: {
-      totalCount: 3,
-      page: 2,
-      transactions: [
-        {
-          Date: "Jan 8th, 2010",
-          Company: "MOMO SUSHI VANCOUVER BC",
-          Ledger: "Interest Income",
-          Amount: "5,000.00"
-        }
-      ]
-    },
-    3: {
-      totalCount: 3,
-      page: 3,
-      transactions: [
-        {
-          Date: "Jan 3rd, 2009",
-          Company: "Transaction - Bench.co",
-          Ledger: "Employment Reimbursment",
-          Amount: "2,000.00"
-        }
-      ]
-    }
-  };
-
   const fetchTransactionsByPage = (page) => {
-    if (page in mockTransactionPages) {
-      return Promise.resolve(mockTransactionPages[page]);
-    } else {
-      return Promise.reject(null);
-    }
+    const url = `${API_BASE_URL}/transactions/${page}.json`;
+
+    return fetch(url)
+      .then(response => response.json())
+      .then(data => data)
+      .catch(err => err);
   };
 
   const fetchAllTransactions = () => {
