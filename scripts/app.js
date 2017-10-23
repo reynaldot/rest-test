b1ba3ad2b1f450920574a1d1767049447c202c48
Reynaldo Tortoledo <reynaldo.tortoledo@gmail.com>
2017-10-23 00:44:57 -0700

    extracted transactions table from index.html into js
    This leaves us with an empty entry point element on the
    document we can show the user different UI states for
    the data we will be loading.

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,29 @@
 APP.Main = ((Transactions, Intl) => {
   'use strict';
 
-  const createTransactionItem = (transaction) => {
+  const containerEl = document.querySelector('.container');
+
+  const createTransactionsTable = (transactions = [], balance = 0) => {
+    const transactionRows = transactions.map(createTransactionRow).join('');
+
+    return `
+      <table class="table transactions">
+        <thead>
+          <tr>
+            <th>Date</th>
+            <th>Company</th>
+            <th>Account</th>
+            <th>${Intl.numberFormatter(balance)}</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${transactionRows}
+        </tbody>
+      </table>
+    `
+  };
+
+  const createTransactionRow = (transaction = {}) => {
     return `
       <tr>
         <td>${transaction.Date}</td>
@@ -12,14 +34,11 @@ APP.Main = ((Transactions, Intl) => {
     `;
   };
 
-  const updateTable = (transactions) => {
-    const totalElement = document.querySelector('.transactions th:last-child');
+  const handleTransactions = (transactions = []) => {
     const balance = Transactions.sumTransactions(transactions);
-    totalElement.textContent = Intl.numberFormatter(balance);
-
-    const transactionsElement = document.querySelector('.transactions > tbody');
-    transactionsElement.innerHTML = transactions.map(createTransactionItem).join('');
+    containerEl.innerHTML = createTransactionsTable(transactions, balance);
   };
 
-  Transactions.fetchAllTransactions().then(transactions => updateTable(transactions));
+  Transactions.fetchAllTransactions()
+    .then(transactions => handleTransactions(transactions))
 })(APP.Transactions, APP.Intl);
